b1834d2facbfdf05ce3f17cf2ad7a34f1d489eed
Stephane de Labrusse <stephdl@de-labrusse.fr>
2017-03-16 19:03:29 +0100

    translation

---
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Madsonic.php
@@ -0,0 +1,7 @@
+<?php
+$L['Madsonic_Title'] = 'Madsonic';
+$L['Madsonic_Description'] = 'Madsonic Music streamer';
+$L['status_label'] = 'enable Madsonic';
+$L['access_green_label'] = 'Private access';
+$L['access_red_label'] = 'Public access';
+$L['SambaUsers_label'] = 'Samba user access to the media folder ';
